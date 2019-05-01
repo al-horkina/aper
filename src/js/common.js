1a594c07e1e18539e5857352af30b66eceeb8ece
Alexandra Horkina <al.horkina@gmail.com>
2019-05-01 15:33:59 +0300

    change js

--- src/js/common.js
+++ src/js/common.js
@@ -1,2 +1,28 @@
-let add = (a,b) => a+b
-console.log(add(2,6))
+(function ($) {
+    $(document).ready(function () {
+        $('.navbar a[href^="#"]').click( function(){
+            var scroll_el = $(this).attr('href');
+            if ($(scroll_el).length != 0) {
+                $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
+            }
+            return false;
+        });
+
+        /*$('#company').scroll(function () {
+            $('#exampleModalLong').modal('toggle')
+        });*/
+
+        $(window).scroll(function(){
+            const offset = $('#company').offset() ;
+            if ($(this).scrollTop() > offset.top && $(this).scrollTop() < (offset.top + 20)  ) {
+                $('#exampleModalLong').modal('show');
+            }
+        });
+
+
+
+
+    })
+})(jQuery);
+
+
