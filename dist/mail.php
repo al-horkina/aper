6e514498ce94740e2ce3be1859e7af4bb98b4bc1
Dmitriy S. Solodukhin <dmitreysergeevich86@gmail.com>
2019-05-17 14:58:18 +0300

    mail

---
+++ dist/mail.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dsolodukhin
+ * Date: 5/17/19
+ * Time: 2:14 PM
+ */
+
+$name  = $_POST['name'];
+$phone = $_POST['phone'];
+$sub   = $_POST['subject'];
+
+$subject = "Заказ с сайта";
+
+$message = 'Телефон: '. $phone .'; Name: ' . $name . '; Форма: ' . $sub;
+
+mail('apercode.system@gmail.com', 'Заказ с сайта', $message);
+
+header('Location: /');
+exit;
